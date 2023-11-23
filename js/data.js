133ce650f29e893874f19690d5c4b19b79867407
Lucas Roncheti <109318375+LucasRoncheti@users.noreply.github.com>
2023-11-23 14:34:00 -0300

    Update Main Pageb
    Atualizado página inicial

--- js/data.js
+++ js/data.js
@@ -13,43 +13,43 @@ var dados = [
    LINK:'https://www.instagram.com/jacksonspub/'
 },
 
-  {   
-   ID:'QWDa',
-   DAY:'07',
-   MONTH:'OUT',
-   HOUSE: 'EVENTO PARTICULAR ',
-   HOUR:'--',
-   LOCATION:'Minas Gerais ',
-   LINK:'#'
-},
+//   {   
+//    ID:'QWDa',
+//    DAY:'07',
+//    MONTH:'OUT',
+//    HOUSE: 'EVENTO PARTICULAR ',
+//    HOUR:'--',
+//    LOCATION:'Minas Gerais ',
+//    LINK:'#'
+// },
 
-{   
-   ID:'QWDa',
-   DAY:'11',
-   MONTH:'OUT',
-   HOUSE: 'Correria ',
-   HOUR:'22',
-   LOCATION:'Vila Velha',
-   LINK:'https://www.instagram.com/correriamusicbar/'
-},
-{   
-   ID:'QWDa',
-   DAY:'14',
-   MONTH:'OUT',
-   HOUSE: 'Deck Bier',
-   HOUR:'20',
-   LOCATION:'Pedra Azul- ES',
-   LINK:'https://www.instagram.com/cervejariadeckbeer/'
-},
-{   
-   ID:'QWDa',
-   DAY:'28',
-   MONTH:'OUT',
-   HOUSE: 'Santinos Pub',
-   HOUR:'20',
-   LOCATION:'Pedra Azul- ES',
-   LINK:'https://www.instagram.com/santinospub/'
-},
+// {   
+//    ID:'QWDa',
+//    DAY:'11',
+//    MONTH:'OUT',
+//    HOUSE: 'Correria ',
+//    HOUR:'22',
+//    LOCATION:'Vila Velha',
+//    LINK:'https://www.instagram.com/correriamusicbar/'
+// },
+// {   
+//    ID:'QWDa',
+//    DAY:'14',
+//    MONTH:'OUT',
+//    HOUSE: 'Deck Bier',
+//    HOUR:'20',
+//    LOCATION:'Pedra Azul- ES',
+//    LINK:'https://www.instagram.com/cervejariadeckbeer/'
+// },
+// {   
+//    ID:'QWDa',
+//    DAY:'28',
+//    MONTH:'OUT',
+//    HOUSE: 'Santinos Pub',
+//    HOUR:'20',
+//    LOCATION:'Pedra Azul- ES',
+//    LINK:'https://www.instagram.com/santinospub/'
+// },
  
 
 
